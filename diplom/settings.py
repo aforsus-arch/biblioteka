37fcb1076967a076f671df1075ed45674a21bb53
aforsus-arch <aforsus@gmail.com>
2026-01-26 21:41:25 +0300

    Fix settings for Railway

--- diplom/settings.py
+++ diplom/settings.py
@@ -32,7 +32,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'biblioteka.apps.BibliotekaConfig'
+    'biblioteka.apps.BibliotekaConfig',
+    'biblioteka'
 ]
 
 MIDDLEWARE = [
